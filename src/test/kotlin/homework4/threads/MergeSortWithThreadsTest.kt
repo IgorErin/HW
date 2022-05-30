463413859232f9517ca462fd959a4389abc7e9b3
IgorErin <erin.igore@yandex.ru>
2022-05-31 02:16:31 +0300

    coroutin/threads ci test

--- src/test/kotlin/homework4/threads/MergeSortWithThreadsTest.kt
+++ src/test/kotlin/homework4/threads/MergeSortWithThreadsTest.kt
@@ -76,10 +76,10 @@ internal class MergeSortWithThreadsTest {
             Arguments.of(smallMediumList, 10),
             Arguments.of(bigMediumList, 10),
             Arguments.of(bigList, 10),
-            Arguments.of(smallList, 15),
-            Arguments.of(smallMediumList, 15),
-            Arguments.of(bigMediumList, 15),
-            Arguments.of(bigList, 15),
+            Arguments.of(smallList, 12),
+            Arguments.of(smallMediumList, 12),
+            Arguments.of(bigMediumList, 12),
+            Arguments.of(bigList, 12),
         )
     }
 }
