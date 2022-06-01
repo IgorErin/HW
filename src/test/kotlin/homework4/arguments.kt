649f88f2dc91649946e89a991ae453afa7f840a3
IgorErin <erin.igore@yandex.ru>
2022-06-01 16:41:20 +0300

    add autogen args

--- src/test/kotlin/homework4/arguments.kt
+++ src/test/kotlin/homework4/arguments.kt
@@ -6,29 +6,11 @@ import org.junit.jupiter.params.provider.Arguments
 import smallList
 import smallMediumList
 
-val autoGenSortTestArguments = listOf(
-    Arguments.of(smallList(), -1),
-    Arguments.of(smallMediumList(), -3),
-    Arguments.of(bigMediumList(), -5),
-    Arguments.of(bigList(), -100),
-    Arguments.of(smallList(), 0),
-    Arguments.of(smallMediumList(), 0),
-    Arguments.of(bigMediumList(), 0),
-    Arguments.of(bigList(), 0),
-    Arguments.of(smallList(), 1),
-    Arguments.of(smallMediumList(), 1),
-    Arguments.of(bigMediumList(), 1),
-    Arguments.of(bigList(), 1),
-    Arguments.of(smallList(), 5),
-    Arguments.of(smallMediumList(), 5),
-    Arguments.of(bigMediumList(), 5),
-    Arguments.of(bigList(), 5),
-    Arguments.of(smallList(), 10),
-    Arguments.of(smallMediumList(), 10),
-    Arguments.of(bigMediumList(), 10),
-    Arguments.of(bigList(), 10),
-    Arguments.of(smallList(), 15),
-    Arguments.of(smallMediumList(), 15),
-    Arguments.of(bigMediumList(), 15),
-    Arguments.of(bigList(), 15),
-)
+private fun testList(index: Int): List<Int> = when {
+    index % 4 == 0 -> smallList()
+    index % 4 == 1 -> smallMediumList()
+    index % 4 == 2 -> bigMediumList()
+    else -> bigList()
+}
+
+val autoGenSortTestArguments = List(12) { Arguments.of(testList(it), it - 2) }
